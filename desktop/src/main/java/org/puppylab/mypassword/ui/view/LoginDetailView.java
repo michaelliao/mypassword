d529f96ac95dbe0affccc13cfb17abffe8b324ea
CryptoMichael <askxuefeng@gmail.com>
2026-04-04 13:32:17 +0800

    add copy/show/hide/large password

--- desktop/src/main/java/org/puppylab/mypassword/ui/view/LoginDetailView.java
+++ desktop/src/main/java/org/puppylab/mypassword/ui/view/LoginDetailView.java
@@ -1,17 +1,41 @@
 package org.puppylab.mypassword.ui.view;
 
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.dnd.Clipboard;
+import org.eclipse.swt.dnd.TextTransfer;
+import org.eclipse.swt.dnd.Transfer;
+import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.graphics.FontData;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.MenuItem;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.ToolTip;
 import org.puppylab.mypassword.core.data.LoginItemData;
 import org.puppylab.mypassword.util.StringUtils;
 
 public class LoginDetailView extends AbstractDetailView<LoginItemData> {
+
+    private static final String MASKED = "\u2022\u2022\u2022\u2022\u2022\u2022\u2022\u2022";
 
     private Label      titleValue;
     private Label      usernameValue;
     private Label      passwordValue;
     private MultiLabel websitesContainer;
     private Label      memoValue;
+
+    private String  plainPassword = "";
+    private boolean passwordVisible = false;
+
+    private MenuItem showHideItem;
+    private Button   copyBtn;
 
     public LoginDetailView(Composite parent) {
         super(parent);
@@ -21,19 +45,128 @@ public class LoginDetailView extends AbstractDetailView<LoginItemData> {
     protected void createFields() {
         titleValue = createField("Title:");
         usernameValue = createField("Username:");
-        passwordValue = createField("Password:");
+        createPasswordField();
         websitesContainer = createMultiValueField("Websites:");
         memoValue = createField("Memo:");
+    }
+
+    private void createPasswordField() {
+        // same 2-column layout as createField:
+        Composite row = new Composite(content, SWT.NONE);
+        row.setLayout(new GridLayout(2, false));
+        row.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+
+        Label lbl = new Label(row, SWT.NONE);
+        lbl.setText("Password:");
+        GridData ld = new GridData();
+        ld.widthHint = 80;
+        lbl.setLayoutData(ld);
+
+        // value cell: password label + buttons in one row
+        Composite valueCell = new Composite(row, SWT.NONE);
+        GridLayout vcl = new GridLayout(3, false);
+        vcl.marginWidth = 0;
+        vcl.marginHeight = 0;
+        valueCell.setLayout(vcl);
+        valueCell.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+
+        passwordValue = new Label(valueCell, SWT.WRAP);
+        passwordValue.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+
+        // Copy button with dropdown arrow:
+        Composite btnGroup = new Composite(valueCell, SWT.NONE);
+        GridLayout btnGl = new GridLayout(2, false);
+        btnGl.marginWidth = 0;
+        btnGl.marginHeight = 0;
+        btnGl.horizontalSpacing = 0;
+        btnGroup.setLayout(btnGl);
+
+        copyBtn = new Button(btnGroup, SWT.PUSH);
+        copyBtn.setText("Copy");
+        copyBtn.addListener(SWT.Selection, e -> copyPassword());
+
+        Button arrowBtn = new Button(btnGroup, SWT.PUSH);
+        arrowBtn.setText("\u25BE");
+
+        Menu menu = new Menu(arrowBtn);
+        showHideItem = new MenuItem(menu, SWT.PUSH);
+        showHideItem.setText("Show Password");
+        showHideItem.addListener(SWT.Selection, e -> togglePasswordVisibility());
+
+        MenuItem largeItem = new MenuItem(menu, SWT.PUSH);
+        largeItem.setText("Show Large Password");
+        largeItem.addListener(SWT.Selection, e -> showLargePassword());
+
+        arrowBtn.addListener(SWT.Selection, e -> {
+            Rectangle rect = arrowBtn.getBounds();
+            Point pt = arrowBtn.getParent().toDisplay(rect.x, rect.y + rect.height);
+            menu.setLocation(pt);
+            menu.setVisible(true);
+        });
     }
 
     @Override
     protected void setData(LoginItemData item) {
         titleValue.setText(StringUtils.normalize(item.data.title));
         usernameValue.setText(StringUtils.normalize(item.data.username));
-        passwordValue.setText(item.data.password != null && !item.data.password.isEmpty()
-                ? "\u2022\u2022\u2022\u2022\u2022\u2022\u2022\u2022"
-                : "");
+        plainPassword = item.data.password != null ? item.data.password : "";
+        passwordVisible = false;
+        passwordValue.setText(plainPassword.isEmpty() ? "" : MASKED);
+        showHideItem.setText("Show Password");
         websitesContainer.setValues(item.data.websites);
         memoValue.setText(StringUtils.normalize(item.data.memo));
+    }
+
+    private void copyPassword() {
+        if (plainPassword.isEmpty()) return;
+        Display display = Display.getCurrent();
+        Clipboard cb = new Clipboard(display);
+        cb.setContents(new Object[] { plainPassword }, new Transfer[] { TextTransfer.getInstance() });
+        cb.dispose();
+        // show tooltip below copy button:
+        ToolTip tip = new ToolTip(composite.getShell(), SWT.BALLOON | SWT.ICON_INFORMATION);
+        tip.setMessage("Copied to clipboard");
+        Rectangle rect = copyBtn.getBounds();
+        Point loc = copyBtn.getParent().toDisplay(rect.x, rect.y + rect.height);
+        tip.setLocation(loc);
+        tip.setVisible(true);
+        display.timerExec(3000, () -> {
+            if (!tip.isDisposed()) tip.dispose();
+        });
+    }
+
+    private void togglePasswordVisibility() {
+        if (plainPassword.isEmpty()) return;
+        passwordVisible = !passwordVisible;
+        passwordValue.setText(passwordVisible ? plainPassword : MASKED);
+        showHideItem.setText(passwordVisible ? "Hide Password" : "Show Password");
+        content.layout(true, true);
+        sc.setMinSize(content.computeSize(sc.getClientArea().width, SWT.DEFAULT));
+    }
+
+    private void showLargePassword() {
+        if (plainPassword.isEmpty()) return;
+        Shell parent = composite.getShell();
+        Shell popup = new Shell(parent, SWT.DIALOG_TRIM);
+        popup.setText("Password");
+        popup.setLayout(new GridLayout(1, false));
+
+        Label label = new Label(popup, SWT.WRAP);
+        label.setText(plainPassword);
+        Font largeFont = new Font(popup.getDisplay(), new FontData("Courier New", 24, SWT.NORMAL));
+        label.setFont(largeFont);
+        GridData gd = new GridData(SWT.FILL, SWT.FILL, true, true);
+        gd.widthHint = 400;
+        label.setLayoutData(gd);
+
+        popup.addListener(SWT.Deactivate, e -> popup.close());
+        popup.addListener(SWT.Dispose, e -> largeFont.dispose());
+
+        popup.pack();
+        // center on parent:
+        Rectangle pb = parent.getBounds();
+        Point ps = popup.getSize();
+        popup.setLocation(pb.x + (pb.width - ps.x) / 2, pb.y + (pb.height - ps.y) / 2);
+        popup.open();
     }
 }
