17c1e2a1544de2740fd6fa07cdf08daadc47b6c1
CryptoMichael <askxuefeng@gmail.com>
2026-04-10 10:25:31 +0800

    auto add space for btn label

--- desktop/src/main/java/org/puppylab/mypassword/util/I18nUtils.java
+++ desktop/src/main/java/org/puppylab/mypassword/util/I18nUtils.java
@@ -8,7 +8,12 @@ public class I18nUtils {
     private static final ResourceBundle bundle = ResourceBundle.getBundle("messages");
 
     public static String i18n(String key) {
-        return bundle.getString(key);
+        boolean isBtn = key.startsWith("btn.") || key.endsWith(".btn") || key.contains(".btn.");
+        String value = bundle.getString(key);
+        if (isBtn) {
+            return "  " + value + "  ";
+        }
+        return value;
     }
 
     public static String i18n(String key, Object... args) {
